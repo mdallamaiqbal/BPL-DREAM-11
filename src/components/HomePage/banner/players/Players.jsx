ab000a14faa62c8c3fa76a7685b5ec05a15d17ad
mdallamaiqbal <bd999plus13@gmail.com>
2026-03-26 23:04:41 +0600

    add toggol btn

--- src/components/HomePage/banner/players/Players.jsx
+++ src/components/HomePage/banner/players/Players.jsx
@@ -1,13 +1,18 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from './AvailablePlayers/AvailablePlayers';
 
 const Players = ({playersPromise}) => {
+    const [toggol,setToggol]=useState('available')
     const players=use(playersPromise);
-    console.log(players);
-    
     return (
-        <div className='container mx-auto'>
-            <p>Players : {players.length}</p>
+        <div className='container mx-auto my-16'>
+          <div className='flex justify-between items-center gap-4 mb-5'>
+            <h2 className='font-bold text-3xl'>Available Players</h2>
+            <div>
+               <button onClick={()=>setToggol('available')} className={`btn ${toggol === 'available' ? "bg-[#E7FE29]" : ""} rounded-r-none rounded-l-xl`}>Available</button>
+               <button onClick={()=>setToggol('selected')} className={`btn ${toggol === 'selected' ? "bg-[#E7FE29]" : ""} rounded-l-none rounded-r-xl`}>Selected(0)</button>
+            </div>
+          </div>
             <AvailablePlayers players={players}/>
         </div>
     );
